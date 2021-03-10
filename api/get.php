b3eeca63ce4284f9b70bf2f807afed0ae6fd9731
Your Name <you@example.com>
2021-03-10 17:35:42 +0200

    Multiple markers and their event clickers

--- api/get.php
+++ api/get.php
@@ -39,36 +39,9 @@ if($tableRowCount > 0){
     }
     http_response_code(200);
 
-    echo json_encode($item_arr);
+    echo json_encode($item_arr['data']);
 } else {
     http_response_code(404);
 
     echo json_encode(array("message" => "No places exist."));
 }
-// Query
-// $result = $post->read();
-// Get row count
-// $num = $result->rowCount();
-// Check if any posts
-// if($num > 0){
-    // Post array
-    /* $posts_arr = array();
-    $posts_arr['data'] = array();
-
-    while($row = $result->fetch(PDO::FETCH_ASSOC)){
-        extract($row);
-        
-        $post_item = array(
-            'id' => $id,
-            'title' => $title,
-            'body' => html_entity_decode($body),
-            'author' => $author,
-            'category_id' => $category_id,
-            'category_name' => $category_name
-        );
-
-        // Push to "data"
-        array_push($posts_arr['data'], $post_item);
-    }
-*/
-    // Turn to JSON & output
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,51 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Pils
-  const pils = { lat: 57.31495175, lng: 25.268124944552586 };
   // The map, centered at Pils
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 7,
-    center: { lat: 56.946285, lng: 24.105078 },
+    center: {
+      lat: 56.946285,
+      lng: 24.105078
+    },
   });
-  // The marker, positioned at Pils
 
-
-  const marker = new google.maps.Marker({
-    position: pils,
-    map: map,
-
+  const infowindow = new google.maps.InfoWindow({
+    content: "TESTING",
   });
+
+
+  const API_url = 'http://localhost/slepnosana/api/get.php';
+  var request = new XMLHttpRequest()
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open('GET', API_url, true)
+
+
+  request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+    for (var i = 0; i < data.length; i++) {
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data[i].latitude, data[i].longitude),
+        map,
+        title: "TITLE HAS TO BE HERE",
+      });
+      var content = "<h3>" + data[i].place_name + '</h3><br>' + "About: " + data[i].about_place;
+
+      var infowindow = new google.maps.InfoWindow()
+
+      google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+        return function () {
+          infowindow.setContent(content);
+          infowindow.open(map, marker);
+        };
+      })(marker, content, infowindow));
+    }
+  }
+  // Send request
+  request.send()
 }
 
-$("#button").click(function() {
+$("#button").click(function () {
 
   let lat = $('input[name=latitude]').val();
   let long = $('input[name=longitude]').val();
@@ -25,23 +53,28 @@ $("#button").click(function() {
   let points = $('input[name=points]').val();
   let about_place = $('input[name=about_place]').val();
 
-  let newPlace = { 'latitude': lat, 'longitude': long,'place_name': place_name, 'points': points, 'about_place': about_place };
+  let newPlace = {
+    'latitude': lat,
+    'longitude': long,
+    'place_name': place_name,
+    'points': points,
+    'about_place': about_place
+  };
 
   console.log(newPlace);
   let saveData = $.ajax({
-      type: 'POST',
-      url: "api/create.php",
-      data: newPlace,
-      dataType: "text",
-      success: function(resultData) {
-          let deita = JSON.parse(resultData);
-          addPlace(deita.id, lat, long, place_name, points, about_place);
-          $('input[name=latitude]').val('');
-          $('input[name=longitude]').val('');
-          $('input[name=place_name]').val('');
-          $('input[name=points]').val('');
-          $('input[name=about_place]').val('');
-      }
+    type: 'POST',
+    url: "api/create.php",
+    data: newPlace,
+    dataType: "text",
+    success: function (resultData) {
+      let deita = JSON.parse(resultData);
+      addPlace(deita.id, lat, long, place_name, points, about_place);
+      $('input[name=latitude]').val('');
+      $('input[name=longitude]').val('');
+      $('input[name=place_name]').val('');
+      $('input[name=points]').val('');
+      $('input[name=about_place]').val('');
+    }
   });
 });
-
