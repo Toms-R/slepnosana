a3f8676d25bf48f4c14cbaa7776af961b50b87d8
Toms-R <riisinjsh8@gmail.com>
2021-03-10 23:40:21 +0200

    fixed sql

--- config/createTable.sql
+++ config/createTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `places` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11)PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `latitude` double NOT NULL,
   `longitude` double NOT NULL,
   `place_name` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
