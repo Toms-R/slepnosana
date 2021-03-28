836d446f72d3280f54b57f09bf37ee7ac181c307
Your Name <you@example.com>
2021-03-28 11:29:44 +0300

    random changes

--- public/object/object_display_data.php
+++ public/object/object_display_data.php
@@ -2,6 +2,8 @@
     // Curl opened
     $curl = curl_init();
 
+    const MY_VARIABLE = 'test';
+
       require_once "curl.config.php";
 
     $response = curl_exec($curl);
@@ -12,20 +14,19 @@
     $API_get_data = json_decode($response);
     $API_get_data_length  = count($API_get_data);
 
-    if(isset($_GET['object'])){
-      if($_GET['object'] > 0 && $_GET['object'] < $API_get_data_length){
+    if (isset($_GET['object'])) { 
+      if ($_GET['object'] > 0 && $_GET['object'] < $API_get_data_length) {
 
       $object_id_number = $_GET['object'];
 
-      print_r("<h1>ID is: " . $API_get_data[$object_id_number-1]->id . "<br>" . 
-                "Latitude is: " . $API_get_data[$object_id_number-1]->latitude . "<br>" . 
-                "Longitude is: " . $API_get_data[$object_id_number-1]->longitude . "<br>" .
-                "Place name is: " . $API_get_data[$object_id_number-1]->place_name . "<br>" .
-                "Points is: " . $API_get_data[$object_id_number-1]->points . "<br>" .
+      print_r("<h1>ID is: " . $API_get_data[$object_id_number - 1]->id . "<br>" . 
+                "Latitude is: " . $API_get_data[$object_id_number - 1]->latitude . "<br>" . 
+                "Longitude is: " . $API_get_data[$object_id_number - 1]->longitude . "<br>" .
+                "Place name is: " . $API_get_data[$object_id_number - 1]->place_name . "<br>" .
+                "Points is: " . $API_get_data[$object_id_number - 1]->points . "<br>" .
                 "About place is: " . $API_get_data[$object_id_number-1]->about_place . "<br>" . 
       "</h1>");
-      }
-      else {
+      } else {
         header("location:object_page.php");
       }
     }
